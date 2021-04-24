30a039e3c58e99019df908ad69ef21511c8b29bc
Matthew <matthew.renner@embeddedalliance.com>
2021-04-24 18:03:02 -0400

    Adding physics collider checkings - it is NOT working but checking it in anyway

---
+++ Assets/Scripts/ECS/Systems/CollisionSystem.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+[UpdateAfter(typeof(EndFramePhysicsSystem))] 
+public class CollisionSystem : JobComponentSystem
+{
+    private BuildPhysicsWorld buildPhysicsWorld;
+    private StepPhysicsWorld stepPhysicsWorld;
+
+    private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+        // commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    [BurstCompile]
+    struct PickupOnTriggerSystemJob : ICollisionEventsJob
+    {
+        [ReadOnly] public ComponentDataFromEntity<Temperature> allTemperatures;
+        // [ReadOnly] public ComponentDataFromEntity<PlayerTag> allPlayers;
+
+        // public EntityCommandBuffer entityCommandBuffer;
+
+        public void Execute(CollisionEvent triggerEvent)
+        {
+            Entity entityA = triggerEvent.EntityA;
+            Entity entityB = triggerEvent.EntityB;
+            Debug.Log("checking!");
+        
+            if (allTemperatures.HasComponent(entityA) && allTemperatures.HasComponent(entityB))
+            {
+                Debug.Log("collided!");
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new PickupOnTriggerSystemJob();
+        job.allTemperatures = GetComponentDataFromEntity<Temperature>(true);
+        // job.entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+
+        JobHandle jobHandle = job.Schedule(
+            stepPhysicsWorld.Simulation, 
+            ref buildPhysicsWorld.PhysicsWorld,
+            inputDependencies);
+
+
+        jobHandle.Complete();
+        // commandBufferSystem.AddJobHandleForProducer(jobHandle);
+        return jobHandle;
+    }
+}
